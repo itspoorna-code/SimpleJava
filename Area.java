f6f58a4fc01e9e7af389ff72729b79fe676becee
Poornachandra S S <itspoorna011@gmail.com>
2025-03-03 15:38:24 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Area{
+    public static void main(String[] args) {
+     
+
+        double radius = 5.21
+
+        double area = Math.PI * Math.pow(radius, 2);
+
+      
+        System.out.println("The area of the circle is: " + area);
+
+    }
+}
